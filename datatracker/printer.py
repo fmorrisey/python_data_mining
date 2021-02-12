911ae67d7321ed281cc927ab3e44f7a11e792ce6
Forrest Morrisey <forrestmorrisey@gmail.com>
2021-02-12 16:51:16 -0600

    fancy new printer function following SOLID

---
+++ datatracker/printer.py
@@ -0,0 +1,10 @@
+class printer:
+    def __init__(self):
+        self.__init__ = True
+
+    def dict(data):
+        for key, value in data.items():
+            print(key, " : $", value)
+
+    def raw(data):
+        print(data)
