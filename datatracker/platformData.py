1884938143a90c808d2b867ab65330f7322a5e94
Forrest Morrisey <forrestmorrisey@gmail.com>
2021-02-12 10:41:53 -0600

    removed test functions in platformData.py

--- datatracker/platformData.py
+++ datatracker/platformData.py
@@ -5,45 +5,48 @@ import collections as col
 
 #names , titles = platformData.copiesPerPlatform(gameData)
 
+
 class platformData(object):
     def __init__(self, game_data):
         self.game_data = game_data
 
-
-    def copiesPer_Dict(game_data):   #Calls multiple functions to return two serpate lists of copies per console
-        numberOfPlatforms = platformData._findUniquePlatforms(game_data, 2013, 2021)
+    # Calls multiple functions to return two serpate lists of copies per console
+    def copiesPer_Dict(game_data):
+        numberOfPlatforms = platformData._findUniquePlatforms(
+            game_data, 2013, 2021)
         platforms = platformData._titlesPer(game_data)
-        topPlatforms = platformData._top_Platforms(platforms, numberOfPlatforms)     #Sorts the data by top platform descending order
-        return topPlatforms #Returns Dict
+        # Sorts the data by top platform descending order
+        topPlatforms = platformData._top_Platforms(
+            platforms, numberOfPlatforms)
+        return topPlatforms  # Returns Dict
 
+    # Calls multiple functions to return two serpate lists of copies per console
+    def copiesPer_Lists(game_data):
+        numberOfPlatforms = platformData._findUniquePlatforms(
+            game_data, 2013, 2021)
+        platforms = platformData._titlesPer(game_data)
+        # Sorts the data by top platform descending order
+        topPlatforms = platformData._top_Platforms(
+            platforms, numberOfPlatforms)
+        platform_names, platform_titles_count = platformData._zip_Platforms(
+            topPlatforms)  # Pulls the dictionary apart
+        return platform_names, platform_titles_count  # Return Lists
 
-    def copiesPer_Lists(game_data):   #Calls multiple functions to return two serpate lists of copies per console
-        numberOfPlatforms = platformData._findUniquePlatforms(game_data, 2013, 2021)
+    # Calls multiple functions to return two serpate lists of copies per console
+    def salesPer_Dict(game_data):
+        numberOfPlatforms = platformData._findUniquePlatforms(
+            game_data, 2013, 2021)
         platforms = platformData._titlesPer(game_data)
-        topPlatforms = platformData._top_Platforms(platforms, numberOfPlatforms)     #Sorts the data by top platform descending order
-        platform_names, platform_titles_count = platformData._zip_Platforms(topPlatforms) #Pulls the dictionary apart
-        return platform_names, platform_titles_count #Return Lists
-    
-    def salesPer_Dict(game_data):   #Calls multiple functions to return two serpate lists of copies per console
-        numberOfPlatforms = platformData._findUniquePlatforms(game_data, 2013, 2021)
-        platforms = platformData._titlesPer(game_data)
-        topPlatforms = platformData._top_Platforms(platforms, numberOfPlatforms)     #Sorts the data by top platform descending order
+        # Sorts the data by top platform descending order
+        topPlatforms = platformData._top_Platforms(
+            platforms, numberOfPlatforms)
         salesPer_Dict = platformData._salesPerV1(game_data, topPlatforms)
-        return topPlatforms #Returns Dict
+        return topPlatforms  # Returns Dict
 
-    
-    def _salesPerV1(game_data, topPlatforms):
-
-    def _titlesPerV2(json_data):
-        platforms = ct(k.globalSales for k in json_data if k.globalSales)globalSales
-        return platforms
-
-    # Counts the number of platforms    
+    # Counts the number of platforms
     def _titlesPer(json_data):
         platforms = ct(k.platform for k in json_data if k.platform)
         return platforms
-
-
 
     def _findUniquePlatforms(json_data, yearMin, yearMax):
         # finds unique platforms taking advantage of the set data structure
@@ -54,8 +57,10 @@ class platformData(object):
             if type(platform.year) != type(None):
                 if yearMin <= platform.year <= yearMax:
                     platform_set.add(platform.platform)
-                else: pass
-            else: pass
+                else:
+                    pass
+            else:
+                pass
 
         unique_platforms = len(platform_set)
         return unique_platforms
@@ -64,7 +69,7 @@ class platformData(object):
         #finds unique publishers taking advantage of the set data structure
         #Constructor
         publisher_set = set((""))
-        
+
         for json_object in json_data:
             if yearMin <= json_object.year <= yearMax:
                 publisher_set.add(json_object.publisher)
@@ -72,20 +77,13 @@ class platformData(object):
         unique_platforms = len(publisher_set)
         return unique_platforms
 
-    def _top_Platforms(platforms, top): 
+    def _top_Platforms(platforms, top):
         top_platforms = set((""))
         top_platforms = dict(ct(platforms).most_common(top))
         return top_platforms
 
-
     def _zip_Platforms(platforms):
-        names , titles = zip(*platforms.items())
+        names, titles = zip(*platforms.items())
         print(names)
         print(titles)
         return names, titles
-
-    
-
-    
-
-   
