e23467ae223e6753694ca04eac3e83d9edcee4a2
Forrest Morrisey <forrestmorrisey@gmail.com>
2021-02-12 17:01:30 -0600

    mainpy now support local api and prints data

--- datatracker/main.py
+++ datatracker/main.py
@@ -1,7 +1,15 @@
 #from platformData import platformData as pfd
 from api import api
 from salesData import salesData as sds
+from printer import printer as pt
 
-if __name__ == '__main__':
-    game_data = api.requests_NameSpace("https://api.dccresource.com/api/games")
+if __name__ == '__main__':    
+    
+    # Use this line for external API Requests
+    #game_data = api.requests_NameSpace("https://api.dccresource.com/api/games")
+    
+    # Use this line to open local data
+    game_data = api.request_local('response.json')
+    
     salesdata = sds.salesPer_Regional(game_data, 2013, 2021)
+    pt.dict(salesdata)
